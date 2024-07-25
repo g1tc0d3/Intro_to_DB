219070d6656ba55c48ce5f439af7731b4cbacb3b
joram kamau <yllejorum@gmail.com>
2024-07-25 21:30:40 +0300

    replicated table creation from task 0

--- task_2.sql
+++ task_2.sql
@@ -1,11 +1,33 @@
-import mysql.connector
 
-mysql.connector.connet()
+CREATE TABLE Books(
+    book_id (PRIMARY KEY)
+    title VARCHAR(130)
+    author_id (Foreign Key referencing Authors table)
+    price DOUBLE
+    publication_date DATE
+    )
 
-IN DATABASE MySQL.pyCREATE TABLE (
-    books
-    authors
-    customers
-    orders
-    order_details
-)
+
+CREATE TABLE Authors(
+    author_id (PRIMARY KEY)
+    author_name VARCHAR(215)
+    )
+
+CREATE TABLE Customers(
+    customer_id (PRIMARY KEY)
+    customer_name VARCHAR(215)
+    email VARCHAR(215)
+    address TEXT
+    )
+
+CREATE TABLE Orders(
+    order_id INT", "customer_id INT", "FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+    order_date DATE
+    )
+
+CREATE TABLE Order_Details(
+    orderdetailid (PRIMARY KEY)
+    order_id (FOREIGN KEY REFERENCING Orders TABLE)
+    book_id (FOREIGN KEY REFERENCING Books TABLE)
+    quantity DOUBLE
+    )
