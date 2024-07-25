2381bf479476e2c0c69c1b17a98a2660a92d60e8
joram kamau <yllejorum@gmail.com>
2024-07-25 17:59:49 +0300

    added caputalization to key word primary key

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,7 +3,7 @@ import mysql.connector.connect()
 CREATE DATABASE IF NOT EXISTS alx_book_store
 
 CREATE TABLE Books(
-    book_id (Primary Key)
+    book_id (PRIMARY KEY)
     title VARCHAR(130)
     author_id (Foreign Key referencing Authors table)
     price DOUBLE
@@ -12,19 +12,19 @@ CREATE TABLE Books(
 
 
 CREATE TABLE Authors(
-    author_id (Primary Key)
+    author_id (PRIMARY KEY)
     author_name VARCHAR(215)
     )
 
 CREATE TABLE Customers(
-    customer_id (Primary Key)
+    customer_id (PRIMARY KEY)
     customer_name VARCHAR(215)
     email VARCHAR(215)
     address TEXT
     )
 
 CREATE TABLE Orders(
-    order_id (Primary Key)
+    order_id (PRIMARY KEY)
     customer_id (Foreign Key referencing Customers table)
     order_date DATE
     )
