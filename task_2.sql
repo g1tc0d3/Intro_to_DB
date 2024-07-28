cabc7171987325843835f65aff1253d156b64a3b
joram kamau <yllejorum@gmail.com>
2024-07-28 21:28:29 +0300

    additional changes made to file including quotations

--- task_2.sql
+++ task_2.sql
@@ -26,6 +26,6 @@ CREATE TABLE Orders(
     )
 
 CREATE TABLE Order_Details(
-    book_id INT, FOREIGN KEY (order_id) REFERENCES Orders(order_id), FOREIGN KEY (book_id) REFERENCES Books(book_id)
+    'book_id INT', 'FOREIGN KEY (order_id)', 'REFERENCES Orders(order_id)', 'FOREIGN KEY (book_id) REFERENCES Books(book_id)'
     quantity DOUBLE
     )
