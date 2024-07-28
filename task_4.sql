6add86fa5bc76f569ec46857157d70cf859520a1
joram kamau <yllejorum@gmail.com>
2024-07-28 19:49:29 +0300

    updated task 4 file

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,3 @@
 import mysql.connector.connect()
 
-list mysql.Books()
+"INFORMATION_SCHEMA.COLUMNS", "SELECT", "COLUMN_NAME", "COLUMN_TYPE", "TABLE_SCHEMA = 'alx_book_store'", "TABLE_NAME = 'Books'"
