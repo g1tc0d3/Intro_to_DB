f3fa02cb9540c518b6d363cae6e69357c94e55f6
joram kamau <yllejorum@gmail.com>
2024-07-28 21:16:27 +0300

    deleted initial values added

--- task_6.sql
+++ task_6.sql
@@ -1,20 +1,5 @@
 INSERT INTO customer (customer_id, customer_name, email, address)
+
 values(2, Blessing Malik, bmalik@sandtech.com, 124 Happiness Ave.),
-(3, Obed Ehoneah, eobed@sandtech.com, 125 Happiness Ave.)
-(4, Nehemiah Kamolu, nkamolu@sandtech.com, 126 Happiness Ave.)
-
-    customer_id = 2
-    customer_name = Blessing Malik
-    email = bmalik@sandtech.com
-    address = 124 Happiness  Ave.
-
-    customer_id = 3
-    customer_name = Obed Ehoneah
-    email = eobed@sandtech.com
-    address = 125 Happiness  Ave.
-
-    customer_id = 4
-    customer_name = Nehemial Kamolu
-    email = nkamolu@sandtech.com
-    address = 126 Happiness  Ave.
-)
+        (3, Obed Ehoneah, eobed@sandtech.com, 125 Happiness Ave.),
+        (4, Nehemiah Kamolu, nkamolu@sandtech.com, 126 Happiness Ave.);
