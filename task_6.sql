e38c68fadd509a938fb17068088952dc28900e9b
joram kamau <yllejorum@gmail.com>
2024-07-28 21:13:03 +0300

    added values to columns available nad specified

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,7 @@
-INSERT INTO customer
-values(
+INSERT INTO customer (customer_id, customer_name, email, address)
+values(2, Blessing Malik, bmalik@sandtech.com, 124 Happiness Ave.),
+(3, Obed Ehoneah, eobed@sandtech.com, 125 Happiness Ave.)
+(4, Nehemiah Kamolu, nkamolu@sandtech.com, 126 Happiness Ave.)
 
     customer_id = 2
     customer_name = Blessing Malik
